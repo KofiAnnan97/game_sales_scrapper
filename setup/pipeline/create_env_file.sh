627684934faf522ec952fc1f61f9a8c05f004e3a
KofiAnnan97 <kofi4annan@gmail.com>
2025-12-06 23:03:56 -0500

    Fix env creation issue

--- setup/pipeline/create_env_file.sh
+++ setup/pipeline/create_env_file.sh
@@ -7,8 +7,8 @@ PROJECT_PATH=$(echo "$PROJECT_PATH")
 # Create file contents
 touch .env
 ENV_STR="STEAM_API_KEY=\"$STEAM_API_KEY\"
-PROJECT_PATH=\"$PROJECT_PATH\""
-TEST_PATH=\"$PROJECT_PATH\src\tests\""
+PROJECT_PATH=\"$PROJECT_PATH\"
+TEST_PATH=\"$PROJECT_PATH/src/tests\""
 
 # Write to .env file
 echo "$ENV_STR" > .env
